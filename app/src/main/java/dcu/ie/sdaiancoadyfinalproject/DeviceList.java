17a1087c53455ca8578c550ea6ff0dd23d41a484
coadyi3 <ian.coady3@mail.dcu.ie>
2020-03-27 12:28:34 +0000

    Recycler View Created and GUI created for it.

---
+++ app/src/main/java/dcu/ie/sdaiancoadyfinalproject/DeviceList.java
@@ -0,0 +1,21 @@
+package dcu.ie.sdaiancoadyfinalproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DeviceList extends AppCompatActivity {
+
+    public DeviceList(){
+        //Required empty public constructor
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rv = findViewById(R.id.);
+    }
+}
