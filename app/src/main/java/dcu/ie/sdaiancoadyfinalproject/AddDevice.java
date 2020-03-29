caaaef1452aa0454f0bc0fbf15d8003fb40c024e
coadyi3 <ian.coady3@mail.dcu.ie>
2020-03-29 13:37:19 +0100

    Added Intent logic to open add Device Activity and created AddDevice class XML file.

---
+++ app/src/main/java/dcu/ie/sdaiancoadyfinalproject/AddDevice.java
@@ -0,0 +1,17 @@
+package dcu.ie.sdaiancoadyfinalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddDevice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_add_device);
+
+    }
+
+
+}
--- app/src/main/java/dcu/ie/sdaiancoadyfinalproject/DeviceList.java
+++ app/src/main/java/dcu/ie/sdaiancoadyfinalproject/DeviceList.java
@@ -1,9 +1,14 @@
 package dcu.ie.sdaiancoadyfinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -19,7 +24,8 @@ public class DeviceList extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        RecyclerView rv = findViewById(R.id.deviceView_view);
+        RecyclerView rv             = findViewById(R.id.deviceView_view);
+        FloatingActionButton fab    = findViewById(R.id.addBtn);
 
         ArrayList<Integer> dDeviceID = new ArrayList<>();
         ArrayList<String> dDeviceModel = new ArrayList<>();
@@ -28,34 +34,18 @@ public class DeviceList extends AppCompatActivity {
         dDeviceID.add(123456);
         dDeviceModel.add("PAX S500");
         dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
-        dDeviceID.add(123456);
-        dDeviceModel.add("PAX S500");
-        dDeviceEnv.add("Cert");
 
         DeviceViewAdapter rvAdapter = new DeviceViewAdapter(getBaseContext(), dDeviceID, dDeviceModel, dDeviceEnv);
         rv.setAdapter(rvAdapter);
         rv.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openAddActivity = new Intent(getApplicationContext(), AddDevice.class);
+                startActivity(openAddActivity);
+            }
+        });
 
     }
 }
